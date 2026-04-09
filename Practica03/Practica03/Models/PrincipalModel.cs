4c3c8a82f952ed32cd540ffb6dfa800c388e57e6
Javier Mendez <88914048+JaviMendez88@users.noreply.github.com>
2026-04-08 18:51:20 -0600

    Set Up - Main Structure
    Creación de vistas (sin layout), modelos y controladores principales de la solución.

---
+++ Practica03/Practica03/Models/PrincipalModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica03.Models
+{
+    public class PrincipalModel
+    {
+    }
+}
---
+++ Practica03/Práctica03 DB.sql
@@ -0,0 +1,32 @@
+-- Práctica 03
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'Practica03DB')
+BEGIN
+    CREATE DATABASE Practica03DB;
+END
+GO
+
+USE Practica03DB;
+GO
+
+
+
+CREATE TABLE PrincipalTB (
+    IdCompra INT IDENTITY(1,1) PRIMARY KEY,
+    Descripcion VARCHAR(100) NOT NULL,
+    MontoTotal DECIMAL(10,2) NOT NULL,
+    Saldo DECIMAL(10,2) NOT NULL,
+    Estado VARCHAR(20) NOT NULL -- (Pendiente / Cancelado)
+);
+
+
+
+CREATE TABLE AbonosTB (
+    IdAbono INT IDENTITY(1,1) PRIMARY KEY,
+    IdCompra INT NOT NULL,
+    Fecha DATETIME NOT NULL DEFAULT GETDATE(),
+    MontoAbono DECIMAL(10,2) NOT NULL,
+
+    CONSTRAINT FK_Abonos_Principal
+        FOREIGN KEY (IdCompra) REFERENCES Principal(IdCompra)
+);
