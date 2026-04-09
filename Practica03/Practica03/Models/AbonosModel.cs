c99499dcf3ce880af3d9af2c4c588c3c00486122
Javier Mendez <88914048+JaviMendez88@users.noreply.github.com>
2026-04-08 19:13:50 -0600

    Modelos
    Set up inicial de Models (AbonosModel y PrincipalModel)

--- Practica03/Practica03/Models/AbonosModel.cs
+++ Practica03/Practica03/Models/AbonosModel.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 
 namespace Practica03.Models
 {
     public class AbonosModel
     {
+
+        public int IdAbono { get; set; }
+        public int IdCompra { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal MontoAbono { get; set; }
+        public PrincipalModel Principal { get; set; }
+
+
     }
 }
--- Practica03/Practica03/Models/PrincipalModel.cs
+++ Practica03/Practica03/Models/PrincipalModel.cs
@@ -7,5 +7,12 @@ namespace Practica03.Models
 {
     public class PrincipalModel
     {
+
+        public int IdCompra { get; set; }
+        public string Descripcion { get; set; }
+        public decimal MontoTotal { get; set; }
+        public decimal Saldo { get; set; }
+        public string Estado { get; set; }
+
     }
 }
