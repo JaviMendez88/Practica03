d1231017d0f80f4ac8ece0bafa5ecfb12b71b374
Raid2021 <raul.pdcps@gmail.com>
2026-04-13 10:08:49 -0600

    Validaciones

--- Practica03/Practica03/Assets/js/Compras.js
+++ Practica03/Practica03/Assets/js/Compras.js
@@ -1,25 +1,45 @@
 ﻿$(document).ready(function () {
 
     $("#IdCompra").change(function () {
-        var idCompra = $(this).val();
+        var idSeleccionado = $(this).val();
 
-        if (idCompra !== "") {
-            $.getJSON("/Abonos/ObtenerSaldo", { id: idCompra }, function (data) {
-                $("#SaldoAnterior").val(data);
+        if (idSeleccionado) {
+            $.ajax({
+                url: '/Abonos/ObtenerSaldo',
+                type: 'GET',
+                data: { id: idSeleccionado },
+                success: function (saldo) {
+                    $("#SaldoAnterior").val(saldo);
+                },
+                error: function () {
+                    alert("Hubo un error de comunicación con el servidor al cargar el saldo.");
+                }
             });
         } else {
-            $("#SaldoAnterior").val("");
+            $("#SaldoAnterior").val('');
         }
     });
 
-    $("form").submit(function () {
-        var saldo = parseFloat($("#SaldoAnterior").val());
-        var abono = parseFloat($("#MontoAbono").val());
+    $("form").submit(function (e) {
+        var saldoAnterior = parseFloat($("#SaldoAnterior").val());
+        var montoAbono = parseFloat($("#MontoAbono").val());
 
-        if (abono > saldo) {
-            alert("El abono no puede ser mayor al saldo");
-            return false;
+        if (isNaN(saldoAnterior)) {
+            alert("Por favor seleccione una compra primero.");
+            e.preventDefault();
+            return;
+        }
+
+        if (isNaN(montoAbono) || montoAbono <= 0) {
+            alert("Debe ingresar un monto de abono mayor a 0.");
+            e.preventDefault();
+            return;
+        }
+
+        if (montoAbono > saldoAnterior) {
+            alert("Error: El monto a abonar (" + montoAbono + ") no puede ser mayor al saldo actual (" + saldoAnterior + ").");
+            $("#MontoAbono").focus();
+            e.preventDefault();
         }
     });
-
 });
